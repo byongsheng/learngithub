641c5940cc3c2b5711efda2e7f83b99726ce76a0
baiyongsheng <821188608@qq.com>
2020-05-04 18:12:53 +0800

    add 2 files

---
+++ startup.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "startup.h"
+
+int my_add(int left, int right)
+{
+	return left + right;
+}
+
+int my_sub(int left, int right)
+{
+	return left + right;
+}
+
+int main(int argc, char *argv[])
+{
+	printf("Hello, Git!\n");
+	return 0;
+}
+
---
+++ startup.h
@@ -0,0 +1,16 @@
+#ifndef _STARTUP_H_
+#define _STARTUP_H_
+
+#ifdef __cplusplus_
+#define extern "C"{
+#endif
+
+int my_add(int left, int right);
+int my_sub(int left, int right);
+
+#ifdef __cplusplus_
+}
+#endif
+
+#endif
+
